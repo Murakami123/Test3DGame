af57d57de91ccb4b380f337e4dfba62a50688ee3
Murakami <snowmanappli123@gmail.com>
2017-11-08 23:22:25 +0900

    左右移動

---
+++ TestGame_20171108/Assets/HakoDora/BarrelController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BarrelController : MonoBehaviour {
+
+	private Rigidbody rigidbody;
+	public float moveSpeed, rollSpeed, jumpSpeed;
+
+	void Start(){
+		rigidbody = GetComponent<Rigidbody>();
+	}
+
+
+//------------------------------------------------------------------
+// 処理
+//------------------------------------------------------------------
+	private float stickLeftX, stickLeftY;
+	private bool  isBtnA_Pushed = false;
+	private void Update(){
+
+		// 暫定横移動
+		var velX = ( Mathf.Abs( stickLeftX ) > 0.3f ) ? stickLeftX * moveSpeed : rigidbody.velocity.x;
+		var velY = ( isBtnA_Pushed )                  ? jumpSpeed              : rigidbody.velocity.y;
+		if( isBtnA_Pushed ) isBtnA_Pushed = false;
+		rigidbody.velocity = new Vector3( velX, velY, 0f );
+
+		// ドラム缶らしく斜めに傾くやつ
+		transform.rotation = Quaternion.AngleAxis( -126f, new Vector3( 1f, 0f, 0f ));
+
+		// 横回転は後回し
+		// transform.Rotate( new Vector3( -126f, 0f, x * rollSpeed ));
+	}
+
+	public void OnStickLeft( float x, float y ){
+		stickLeftX = x;
+		stickLeftY = y;
+	}
+
+	public void OnBtnA_Push(){
+		isBtnA_Pushed = true;
+	}
+
+
+	public void Open(){
+
+	}
+
+	public void Close(){
+		
+	}
+
+}
---
+++ TestGame_20171108/Assets/HakoDora/SceneManager_Battle.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class SceneManager_Battle : MonoBehaviour {
+
+//------------------------------------------------------------------
+// シングルトン
+//------------------------------------------------------------------
+	public static SceneManager_Battle Instance;
+	private SceneManager_Battle(){
+		if( Instance == null ) Instance = this;
+	}
+
+//------------------------------------------------------------------
+// 初期化
+//------------------------------------------------------------------
+	public void Initialize(){
+	}
+
+	[SerializeField] XboxControllerManager xboxControlManager;
+	[SerializeField] XBoxInputType InputMode_charaMove;
+	[SerializeField] XBoxInputType InputMode_windowControl; 
+	[SerializeField] XBoxInputType InputMode_pause; 	
+	public int SceneMode;
+	void Update(){
+		Debug.Log("SceneMode:" + SceneMode );
+		switch( SceneMode ){
+			case  1: 
+				xboxControlManager.InputController( InputMode_charaMove ); 
+			break;
+			case  2:
+				xboxControlManager.InputController( InputMode_windowControl ); 
+			break;
+			case  3:
+				xboxControlManager.InputController( InputMode_pause ); 
+			break;
+			default: 
+				xboxControlManager.InputController( InputMode_charaMove ); 
+			break;
+		}
+		
+	}
+
+	public void OnInputButtonA(){
+		Debug.Log("ButtonA");
+	}
+	public void OnInputButtonB(){
+		Debug.Log("ButtonB");
+	}
+	public void OnInputButtonX(){
+		Debug.Log("ButtonX");
+	}
+	public void OnInputButtonY(){
+		Debug.Log("ButtonY");
+	}
+	public void OnInputButtonLB(){
+		Debug.Log("ButtonLB");
+	}
+	public void OnInputButtonRB(){
+		Debug.Log("ButtonRB");
+	}
+	public void OnInputButtonBACK(){
+		Debug.Log("ButtonBACK");
+	}
+	public void OnInputButtonSTART(){
+		Debug.Log("ButtonSTART");
+	}
+	public void OnInputStickLeftPush(){
+		Debug.Log("StickLeftPush");
+	}
+	public void OnInpuStickRightPush(){
+		Debug.Log("StickRightPush");
+	}
+	public void OnInputStick( float x, float y ){
+		Debug.Log("スティック入力！　x:" + x + ", y:" + y );
+	}
+
+
+
+//------------------------------------------------------------------
+// 処理
+//------------------------------------------------------------------
+	public void Open(){
+
+	}
+
+	public void Close(){
+		
+	}
+
+//------------------------------------------------------------------
+// ボタン
+//------------------------------------------------------------------
+	public void OnOKTouch(){
+
+	}
+
+}
